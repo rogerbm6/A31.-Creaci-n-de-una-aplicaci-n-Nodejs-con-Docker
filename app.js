93c0eb9e392bd3181a3691e349dccdd0355a3a59
root <root@debian.rogercrack.local>
2020-02-19 07:29:45 -0500

    Imagen cambiada, sin express generator

--- app.js
+++ app.js
@@ -1,46 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const path = __dirname + '/views/';
+const port = 8080;
 
 
-//
-
-//
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
 });
 
-module.exports = app;
+router.get('/sharks', function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
